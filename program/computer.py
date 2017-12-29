ec2ef4003cb8a4c66ef3aaa142bb84865a0f06db
Isac Moura <contatoisacmoura@outlook.com>
2017-12-29 02:02:40 -0300

    Upload da pasta program

---
+++ program/computer.py
@@ -0,0 +1,22 @@
+import platform
+
+def os():
+    return platform.uname().system
+
+def osVersion():
+    return platform.uname().version
+
+def name():
+    return platform.uname().node
+
+def arch():
+    return platform.uname().machine
+
+def cpu():
+    platform.uname().processor
+
+def distro():
+    if os() == "Linux":
+        return platform.linux_distribution()
+    else:
+        return os()
